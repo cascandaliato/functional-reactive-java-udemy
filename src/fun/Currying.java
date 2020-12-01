3249d5f6884314a87d0e56b725a055961c32f000
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-01 09:53:12 +0100

    currying, tail call optimization

---
+++ src/fun/Currying.java
@@ -0,0 +1,20 @@
+package fun;
+
+import java.util.function.Function;
+
+public class Currying {
+  public static void main(String[] args) {
+    Function<Integer, Function<Integer, Integer>> fun1 = u -> v -> u + v;
+
+    Function<Integer, Integer> fun2 = fun1.apply(1);
+
+    Integer sum = fun2.apply(2);
+
+    System.out.println(sum);
+
+    System.out.println(fun1.apply(1).apply(2));
+
+    Function<Integer, Function<Integer, Function<Integer, Integer>>> f = u -> v -> w -> u + v + w;
+    System.out.println(f.apply(1).apply(2).apply(3));
+  }
+}
---
+++ src/fun/TailCallOptimization.java
@@ -0,0 +1,17 @@
+package fun;
+
+public class TailCallOptimization {
+  public static void main(String[] args) {
+    //
+  }
+
+  public static long reFact(int n) {
+    if (n <= 1) return 1;
+    return n * reFact(n - 1);
+  }
+
+  public static long tailReFact(int n, int a) {
+    if (n <= 1) return a;
+    return tailReFact(n - 1, n * a);
+  }
+}
